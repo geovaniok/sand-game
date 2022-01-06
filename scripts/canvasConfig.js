032676568d65c8c8a0def074d401207e66b68b31
Echo <58097612+3kh0@users.noreply.github.com>
2022-01-06 10:11:52 -0500

    Update canvasConfig.js

--- scripts/canvasConfig.js
+++ scripts/canvasConfig.js
@@ -1,22 +1,3 @@
-/*
- * Global game parameters for the primary canvas.
- *
- * Copyright (C) 2020, Josh Don
- *
- * Project Sand is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Project Sand is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 const __max_width = 560;
 const __max_height = 480;
 
